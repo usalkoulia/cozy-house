63eee252fc5ef8c4f961970bc27164608002b61a
Ulia Usalko <usalko.ulia@gmail.com>
2018-04-24 19:30:18 +0300

    Работа над оживлением слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,19 @@
-var lis = document.getElementsByTagName("li");
-for (var i = 0; i < lis.length; i++) {
- lis[i].style.position = "relative";
- var span = document.createElement("span");
- span.style.cssText = "position:absolute;left:0;top:0";
- span.innerHTML = i + 1;
- lis[i].appendChild(span);
-}
+var width = 270; // ширина изображения
+var count = 1; // количество изображений
+var list = document.querySelector(".slider__list");
+var listElems = document.querySelectorAll(".slider__item");
+var position = 0; // текущий сдвиг влево
 
-var next = carousel.querySelector(".slider__button-arrow--right");
+var next = document.querySelector(".slider__button-arrow--right");
 
 next.addEventListener("click", function() {
-  // сдвиг вправо
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
   position = Math.max(position - width * count, -width * (listElems.length - count));
   list.style.marginLeft = position + "px";
 });
 
-var prew = carousel.querySelector(".slider__button-arrow--left");
+var prew = document.querySelector(".slider__button-arrow--left");
 
 prew.addEventListener("click", function() {
-  // сдвиг вправо
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
   position = Math.max(position - width * count, -width * (listElems.length - count));
   list.style.marginRight = position + "px";
 });
