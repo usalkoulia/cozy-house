1d35e9ee0b097ed9bfcab73ebf343b6f2a517a9f
Ulia Usalko <usalko.ulia@gmail.com>
2018-04-24 19:54:32 +0300

    Работа над оживлением слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
-var width = 270; // ширина изображения
-var count = 1; // количество изображений
+var width = 320; // ширина изображения
+var count = 3; // количество изображений
 var list = document.querySelector(".slider__list");
 var listElems = document.querySelectorAll(".slider__item");
 var position = 0; // текущий сдвиг влево
@@ -7,7 +7,7 @@ var position = 0; // текущий сдвиг влево
 var next = document.querySelector(".slider__button-arrow--right");
 
 next.addEventListener("click", function() {
-  position = Math.max(position - width * count, -width * (listElems.length - count));
+  position = Math.min(position + width * count, 0)
   list.style.marginLeft = position + "px";
 });
 
