abe3b4a3d35f6a5da2f648be56a113e7581d8d26
Ulia Usalko <usalko.ulia@gmail.com>
2018-04-24 20:01:59 +0300

    Слайдер жив ура

--- source/js/main.js
+++ source/js/main.js
@@ -7,13 +7,13 @@ var position = 0; // текущий сдвиг влево
 var next = document.querySelector(".slider__button-arrow--right");
 
 next.addEventListener("click", function() {
-  position = Math.min(position + width * count, 0)
+  position = Math.max(position - width * count, -width * (listElems.length - count));
   list.style.marginLeft = position + "px";
 });
 
 var prew = document.querySelector(".slider__button-arrow--left");
 
 prew.addEventListener("click", function() {
-  position = Math.max(position - width * count, -width * (listElems.length - count));
-  list.style.marginRight = position + "px";
+  position = Math.min(position + width * count, 0)
+  list.style.marginLeft = position + "px";
 });
