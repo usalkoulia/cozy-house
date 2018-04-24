7393b7443ba12288123c4fa07923a17dddc6b13c
Ulia Usalko <usalko.ulia@gmail.com>
2018-04-24 19:07:47 +0300

    Работа над переключением слайдов

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,26 @@
-var lis = document.getElementsByTagName('li');
+var lis = document.getElementsByTagName("li");
 for (var i = 0; i < lis.length; i++) {
- lis[i].style.position = 'relative';
- var span = document.createElement('span');
- span.style.cssText = 'position:absolute;left:0;top:0';
+ lis[i].style.position = "relative";
+ var span = document.createElement("span");
+ span.style.cssText = "position:absolute;left:0;top:0";
  span.innerHTML = i + 1;
  lis[i].appendChild(span);
 }
+
+var next = carousel.querySelector(".slider__button-arrow--right");
+
+next.addEventListener("click", function() {
+  // сдвиг вправо
+  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  position = Math.max(position - width * count, -width * (listElems.length - count));
+  list.style.marginLeft = position + "px";
+});
+
+var prew = carousel.querySelector(".slider__button-arrow--left");
+
+prew.addEventListener("click", function() {
+  // сдвиг вправо
+  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  position = Math.max(position - width * count, -width * (listElems.length - count));
+  list.style.marginRight = position + "px";
+});
